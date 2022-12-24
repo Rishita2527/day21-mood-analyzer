5fb4cfd45c4f8742ffca2b7d615ed842b2d5222c
rishita1234 <rishita123@gmail.com>
2022-12-24 20:16:17 +0530

    Added: Program to throw null or empty mood custom exception

--- src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -1,24 +1,34 @@
 package com.bridgelabz.moodanalyzer;
 
 public class MoodAnalyzer {
-    private String message;
-    //Default Constructor
-    public MoodAnalyzer() {
-    }
-    //Parameterized Constructor
+    public String message;
+      //constructor is used to pass the mood message
+
+      //@param message:passed as parameter in constructor
+
     public MoodAnalyzer(String message) {
         this.message = message;
+        analyzeMood();
     }
 
-    //Handle NULLPOINTER Exception using try-catch block
+
+     // method is used to return either sad or happy
+     //@return sad/happy message
+
     public String analyzeMood() {
+        /**
+         * try :where the implementation is done,so it return either sad/happy
+         * if message is entered is invalid, then null pointer exception is occured and catch block handles it
+         */
         try {
-            if (message.contains("sad"))
+            if (message.contains("sad")) {
                 return "SAD";
-            else
-                return "HAPPY";
-        } catch(Exception e) {
+            }return "HAPPY";
+
+        } catch (NullPointerException e) {
             return "HAPPY";
         }
     }
 }
+
+
--- src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyserTest.java
+++ src/test/java/com/bridgelabz/moodanalyzer/MoodAnalyserTest.java
@@ -5,15 +5,15 @@ import org.junit.jupiter.api.Test;
     public class MoodAnalyserTest {
 
         @Test
-        public void givenMessage_WhenProper_ShouldReturnSad() {
-            MoodAnalyzer moodAnalyser=new MoodAnalyzer();
-            String result=moodAnalyser.analyzeMood("I am in sad mood");
+        public void givenMessage_WhenProper_ShouldReturnSad() { //TC 1.1 "I am in sad mood"
+            MoodAnalyzer moodAnalyser=new MoodAnalyzer("I am in sad mood");
+            String result=moodAnalyser.analyzeMood();
             Assertions.assertEquals("SAD", result);
         }
         @Test
-        public void givenMessage_WhenProper_ShouldReturnHappy() {
-            MoodAnalyzer moodAnalyser = new MoodAnalyzer();
-            String result1 = moodAnalyser.analyzeMood("I am in Happy mood");
+        public void givenMessage_WhenProper_ShouldReturnHappy() { //TC 1.1 "I am in happy mood"
+            MoodAnalyzer moodAnalyser = new MoodAnalyzer("I am in Happy mood");
+            String result1 = moodAnalyser.analyzeMood();
             Assertions.assertEquals("Happy", result1);
         }
 }
