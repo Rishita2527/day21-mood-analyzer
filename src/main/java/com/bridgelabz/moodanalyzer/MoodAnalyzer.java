f63a8d56b1405830e9c75228de3f3c5d80886c58
rishita1234 <rishita123@gmail.com>
2022-12-24 19:23:03 +0530

    Added: Ability to add mood message in constructor

--- src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
+++ src/main/java/com/bridgelabz/moodanalyzer/MoodAnalyzer.java
@@ -1,16 +1,18 @@
 package com.bridgelabz.moodanalyzer;
 
 public class MoodAnalyzer {
-    public String analyzeMood(String message){
-        if (message.contains("sad")){
+    public String message;
+
+    public MoodAnalyzer(String message){
+        this.message = message;
+        analyzeMood();
+    }
+
+    public String analyzeMood() {
+        if (message.contains("sad"))
             return "SAD";
-        }
-        else if(message.contains("happy")){
+        else
             return "HAPPY";
-        }
-        else {
-            return "";
-        }
     }
 
 }
